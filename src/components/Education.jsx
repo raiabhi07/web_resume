6ba6fb962b71bf3b7aa29fc130d15a6bd305fb6c
Abhishek Rai <90126255+raiabhi07@users.noreply.github.com>
2023-09-05 08:03:32 +0530

    Add files via upload

---
+++ src/components/Education.jsx
@@ -0,0 +1,21 @@
+
+export const education = [
+    {
+        Institute: "Raj Kumar Goel Institute of Technology",
+        Degree: "Bachelor of Technology in Information Technology",
+        Year: "2020 - 2024",
+        Marks: "75%"
+    },
+    {
+        Institute: "Sunbeam School Ghazipur",
+        Degree: "12th CBSE PCM",
+        Year: "2020",
+        Marks: "85.8%"
+    },
+    {
+        Institute: "Sunbeam School Ghazipur",
+        Degree: "10th CBSE",
+        Year: "2018",
+        Marks: "76%"
+    }
+];
---
+++ src/components/Experience.jsx
@@ -0,0 +1,11 @@
+import logo1 from '../Images/ment.png'
+export const experience = [
+    {
+        img: logo1,
+        header: "Brain Mentors",
+        subHeader: "Full stack developer Trainee",
+        subTitleHeader: "June 2023 - Present",
+        para1: "My role is to made a Notes Taking Full Stack App",
+        para2: "Made partial page CRUD operations",
+    }
+];
